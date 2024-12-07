a4a9307994cb307d774077bb7b4bfc02ff1f8ef1
brian <brianbarbieri@hotmail.com>
2024-12-07 11:21:12 +0100

    Day 6 sol 2 add non working sol with inf loop that needs fix

--- day6/main.go
+++ day6/main.go
@@ -9,13 +9,16 @@ import (
 
 func main() {
 	example_map := readExample()
-	input_map := readInput()
+	// input_map := readInput()
 
-	answer := solution1(example_map)
-	fmt.Println("Answer for example solution 1: ", answer)
+	// answer := solution1(example_map)
+	// fmt.Println("Answer for example solution 1: ", answer)
 
-	answer = solution1(input_map)
-	fmt.Println("Answer for input solution 1: ", answer)
+	// answer = solution1(input_map)
+	// fmt.Println("Answer for input solution 1: ", answer)
+
+	answer := solution2(example_map)
+	fmt.Println("Answer for example solution 2: ", answer)
 
 }
 
@@ -40,7 +43,29 @@ func solution1(data [][]string) int {
 
 func solution2(data [][]string) int {
 
+	STARTCHAR, POSTIONCHAR := "^", "X"
+	x, y := 0, 0
+
+	data, x, y = findStart(data, STARTCHAR, POSTIONCHAR)
+
+	// fmt.Println(x, y)
+
+	data = walkMap(data, x, y)
+
+	// printMap(data)
+
 	// first get all locations of current path minus starting location
+	walkedPositions := getWalkedPositions(data, POSTIONCHAR, x, y)
+
+	// fmt.Println(walkedPositions)
+
+	total := 0
+	for i := 0; i < len(walkedPositions); i++ {
+		fmt.Println(i)
+
+		total += determineCircle(data, walkedPositions[i], x, y)
+	}
+
 	// iterate over path and add object to current iteration
 
 	// log step to array one (a1)
@@ -49,10 +74,97 @@ func solution2(data [][]string) int {
 	// if add now to second array
 	// if a1[index] != a2[index] reset arrays and add path again to a1
 
+	// if a1[0] == a2[0] and a1[1] == a2[1] than we go in circles
+
 	// a1 = 1,2,3,4,5
 	// a2 = 1,2
 
+	return total
+}
+
+func determineCircle(data [][]string, position []int, x, y int) int {
+	data[position[0]][position[1]] = "#"
+	walkedRecord := make([][]int, 0)
+	BLOCKCHAR, POSTIONCHAR := "#", "X"
+	secondRound := false
+	nextStep := []int{0, 0}
+
+	direction := "up"
+	for {
+
+		switch direction {
+		case "up":
+			x--
+		case "down":
+			x++
+		case "left":
+			y--
+		case "right":
+			y++
+		}
+		if isOutside(data, x, y) {
+			break
+		}
+		if data[x][y] == BLOCKCHAR {
+			switch direction {
+			case "up":
+				x++
+				direction = "right"
+			case "down":
+				x--
+				direction = "left"
+			case "left":
+				y++
+				direction = "up"
+			case "right":
+				y--
+				direction = "down"
+			}
+		} else {
+			data[x][y] = POSTIONCHAR
+			if secondRound {
+				if nextStep[0] == x && nextStep[1] == y {
+					return 1
+				} else {
+					secondRound = false
+				}
+			} else {
+				walkedRecord = append(walkedRecord, []int{x, y})
+				isFound, a := findXYInRecord(walkedRecord, x, y)
+				if isFound {
+					nextStep = a
+					secondRound = true
+				}
+			}
+
+		}
+	}
+
+	// walk map but record all positions by adding x and y as "x_Y" to array if x_y already in list
+	//walk once more and see if it matches next x_y in the list fi so we walk a circle
+
 	return 0
+}
+
+func findXYInRecord(record [][]int, x, y int) (bool, []int) {
+	for i := 0; i < len(record)-1; i++ {
+		if record[i][0] == x && record[i][1] == y {
+			return true, record[i+1]
+		}
+	}
+	return false, nil
+}
+
+func getWalkedPositions(data [][]string, positionChar string, startPositionX int, startPositionY int) [][]int {
+	positions := make([][]int, 0)
+	for i := 0; i < len(data); i++ {
+		for j := 0; j < len(data[i]); j++ {
+			if data[i][j] == positionChar && !(i == startPositionX && j == startPositionY) {
+				positions = append(positions, []int{i, j})
+			}
+		}
+	}
+	return positions
 }
 
 func findStart(data [][]string, startChar string, positionChar string) ([][]string, int, int) {
